69646cbc7d69952b54b3273b82ffef0b03775858
Kumar Gaurav <kgaurav@adobe.com>
2019-12-30 11:40:35 +0530

    Aggregation Framework

---
+++ aggregationFramework/mongodbCommand_2.js
@@ -0,0 +1,5 @@
+db.persons.aggregate([
+  { $match: { "dob.age": { $gt: 50 } } },
+  { $group: { _id: {"gender": "$gender"}, "totalPersons": { $sum: 1 }, "averageAge": {$avg: "$dob.age"} } },
+  { $sort: {"totalPersons": 1} }
+])
